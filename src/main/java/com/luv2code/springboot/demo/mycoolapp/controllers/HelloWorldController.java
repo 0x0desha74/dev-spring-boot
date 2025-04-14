48261f3fa21db889c1065b73d02bbb27604ee5e3
0x0desha74 <mudesha74@gmail.com>
2025-04-14 07:53:10 +0200

    spring acyuator

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/controllers/HelloWorldController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.luv2code.springboot.demo.mycoolapp.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/")
     public String Hello(){
-        return "Hello, World!";
+        return "Hello, World From Egypt!";
+    }
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello from hello";
+    }
+    @GetMapping("/newone/{name}")
+    public String Hello(@PathVariable String name ){
+        return "Hello "+name;
     }
 }
